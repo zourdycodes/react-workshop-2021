c087ac2f691a2bfe64cb2e839b31779d821b50cb
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-07-25 16:53:08 +0700

    useEffect rendering d

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { UseStateBasics } from "./tutorial/1-useState/";
-import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Setup from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
 
 export const App = () => {
   return (
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,77 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/zourdycodes";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [user, setUser] = useState("default");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(url)
+        .then((response) => {
+          if (
+            !response.ok &&
+            response.status >= 400 &&
+            response.status <= 600
+          ) {
+            return setError((error) => !error);
+          }
+
+          return response.json();
+        })
+        .then((data) => {
+          setUser(data);
+          setLoading((loading) => !loading);
+        });
+
+      // const data = await response.json()
+      // setUser(data);
+      // setLoading((loading) => !loading);
+      return response;
+    };
+
+    fetchUser();
+  }, []);
+
+  console.log(error);
+
+  if (error) {
+    return (
+      <React.Fragment>
+        <h4>
+          Error, No Data fetched! the request call is maybe forbidden or your
+          API endpoint is wrong
+        </h4>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <React.Fragment>
+      <h2>multiple returns</h2>
+      {loading ? (
+        <h2>Loading ...</h2>
+      ) : (
+        <ul className="users">
+          <li key={user.id}>
+            <img src={user.avatar_url} alt={user.name} />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                marginTop: "15px",
+              }}
+            >
+              <h4>{user.name}</h4>
+              <p>{user.location}</p>
+            </div>
+          </li>
+        </ul>
+      )}
+    </React.Fragment>
+  );
 };
 
 export default MultipleReturns;
