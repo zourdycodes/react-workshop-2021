fa6bbe4bf323350ed3b432158fc8df484c708dac
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-05 05:44:16 +0700

    useRef exercise

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { UseStateBasics } from "./tutorial/1-useState/";
-import Setup from "./tutorial/4-forms/setup/2-multiple-inputs";
+import Setup from "./tutorial/5-useRef/setup/1-useRef-basics";
 
 export const App = () => {
   return (
