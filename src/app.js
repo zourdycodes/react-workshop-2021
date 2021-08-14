dfce6fed8064a844949fa4a46b69e1afce07a148
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-08-14 21:10:13 +0700

    continuing create the curriculum for course learning --> UNHCR Debrecen Volunteer

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import { UseStateBasics } from "./tutorial/1-useState/";
-import Setup from "./tutorial/9-custom-hooks/setup/1-fetch-example";
+import Setup from "./tutorial/10-prop-types/setup";
 
 export const App = () => {
   return (
--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Product = () => {
-  return <article className='product'>single product</article>;
+  return <article className="product">single product</article>;
 };
 
 export default Product;
